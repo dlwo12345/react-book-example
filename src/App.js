821083acede84207a34b9d35b7b35977a9ed45b7
hong <dlwo12345@naver.com>
2019-03-01 15:28:01 +0900

    Validation예제 추가

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Component, Fragment } from 'react';
 import './App.css';
 import MyComponent from './component/MyComponent';
 import EventPractice from './component/EventPractice';
+import ValidationSample from './component/ValidationSample';
 
 class App extends Component {
 
   render() {
     return (
-      // <Fragment>
-      //   <MyComponent name="강낭콩" />
-      //   <MyComponent />
-      // </Fragment>
       <Fragment>
-        <EventPractice />
+        {/* <MyComponent name="강낭콩" /> */}
+        {/* <MyComponent /> */}
+        {/* <EventPractice /> */}
+        <ValidationSample />
       </Fragment>
     );
   }
