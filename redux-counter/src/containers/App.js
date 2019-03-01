197106b39b599897c0952a5e754925e07a989ccf
hong <dlwo12345@naver.com>
2019-03-02 00:29:07 +0900

    App.js 변경

--- redux-counter/src/containers/App.js
+++ redux-counter/src/containers/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import Counter from '../components/Counter';
+import Buttons from '../components/Buttons';
 import CounterContainer from './CounterContainer';
+import CounterListContainer from './CounterListContainer';
+import getRandomColor from '../lib/getRandomColor';
+
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class App extends Component {
     render() {
+        const { onCreate, onRemove } = this.props;
         return (
-            <div>
+            <div className="App">
                 {/* <Counter /> */}
-                <CounterContainer />
+                {/* <CounterContainer /> */}
+                <Buttons
+                    onCreate={onCreate}
+                    onRemove={onRemove}
+                ></Buttons>
+                <CounterListContainer />
             </div>
         );
     }
 }
 
-export default App;
+const mapToDispatch = (dispatch) => ({
+    onCreate: () => dispatch(actions.create(getRandomColor())),
+    onRemove: () => dispatch(actions.remove())
+})
+
+export default connect(null, mapToDispatch)(App);
